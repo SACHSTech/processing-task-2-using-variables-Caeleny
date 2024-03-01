c589e14fef47022c100605b62ffac1af4a45c1c3
Caeleny <caelen.yeung25@ycdsbk12.ca>
2024-03-01 12:41:47 -0500

    submit

--- Sketch.java
+++ Sketch.java
@@ -2,35 +2,48 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
   public void settings() {
-	// put your size call here
-    size(400, 400);
-  }
-
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
-  public void setup() {
-    background(210, 255, 173);
-  }
-
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
+    // Background size
+      size(800, 800);
+    }
   
-  // define other methods down here.
+    // Background color
+    public void setup() {
+      background(67, 170, 239);
+    }
+  
+    // Different shapes
+    public void draw() {
+      
+      // House
+      fill(255, 220, 150);
+      rect(width/10, height/2, (float)(width*0.75), height/2);
+
+      // Door
+      fill(66, 69, 62);
+      rect((float)(width*0.75), (float)(width*0.825), width/10, (float)(width*0.175));
+  
+      // Window
+      fill(120, 205, 255);
+      rect((float)(width*0.525), (float)(height*0.575), width/4, (float)(height*0.175));
+  
+      // Window 2
+      fill(120, 205, 255);
+      rect((float)(width*0.175), (float)(width*0.575), width/4, (float)(width*0.175));
+  
+      // Window 3 
+      fill(120, 205, 255);
+      rect((float)(width*0.175), (float)(width*0.8), (float)(width*0.375), (float)(width*0.175));
+  
+      // Roof
+      fill(163, 112, 49);
+      triangle((float)(width*0.075), height/2, width/2, height/4, (float)(width*0.875), height/2);
+    
+      // Sun
+      fill(255, 242, 0);
+      ellipse(width/8,height/8, width/4, height/4); 
+
+      
+    }
+  
 }
